ec4525942119213c9cca9c9cb1b0aa593a940d7c
qTeePie <isabel@chaparian.no>
2025-07-25 23:59:52 +0200

    naming standards 🔠 noted / 2

--- common_concepts/control_flow/src/main.rs
+++ common_concepts/control_flow/src/main.rs
@@ -7,8 +7,42 @@
     let number = if condition { 5 } else { 6 }; ✅ `number` binds to the return value
     let number = if condition { 5 } else { "six" }; ❌ mismatch in datatype! Rustie says, no go!
 
+    Loops returns value with break statement, eg. break result;
+    Remember how they said blocks return last expression as long as there is no ;?
 
+    Loops return values via break, not by just ending with an expression.
+    Break doesn't need to be any place in the loop, end, middle, start,
+    wherever in the block it is, attach a value and its returned.
+
+    “I’m out 💨 — oh, and here’s the value: 🧾 result;”
 */
+
+fn looper_nested() {
+    let mut count = 0;
+
+    // sets loop label
+    let remaining = 'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            // taking a coupe rounds until count is 2, then breaks outer loop
+            if count == 2 {
+                // break 'counting_up; // returns () type unit
+                break 'counting_up remaining; // <-- this returns value
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    };
+
+    println!("End count = {count}\n End remaining {remaining}");
+}
 
 fn looper() -> i32 {
     // mutable variabøe
@@ -20,7 +54,7 @@ fn looper() -> i32 {
         if counter == 10 {
             break counter * 2;
         }
-    } // no ; here cause we want the functiont to return this value (beak return)
+    } // no ; here cause we want the functiont to return this value (attached to break)
 }
 
 fn let_if(condition: bool) -> i32 {
@@ -32,10 +66,12 @@ fn let_if(condition: bool) -> i32 {
 }
 
 fn main() {
-    let result = letIf(true);
+    let result = let_if(true);
     println!("The value of number is: {result}");
 
     // Shadowing
     let result = looper();
-    println!("The result is {result}");
+    println!("The result from loop is {result}");
+
+    looper_nested();
 }
